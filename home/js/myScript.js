ff4b1119b970138570c0d8b6b46f8d78883eea2a
Christian Esguerra <ciesguerra2@student.apc.edu.ph>
2024-12-17 09:07:25 +0800

    Tweaked the HTML DOM code

--- home/js/myScript.js
+++ home/js/myScript.js
@@ -3,6 +3,6 @@ function myFunction() {
 }
 
 function myFunction2() {
-    const element = document.getElementsByTagName("br");
-    document.getElementById("demo").innerHTML = 'The text in first  <br> tag (index 0) is: ' + element[0].innerHTML;    
+    const element = document.getElementsByTagName("p");
+    document.getElementById("demo").innerHTML = '<br> The text in first paragraph (index 0) is: <br>' + element[0].innerHTML;
 }
