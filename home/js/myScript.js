9cbf09046df4c5d7346bee610e7df0b3abb92a98
Christian Esguerra <ciesguerra2@student.apc.edu.ph>
2024-12-19 21:10:58 +0800

    Redid entire function2 to be sure

--- home/js/myScript.js
+++ home/js/myScript.js
@@ -3,10 +3,11 @@ function myFunction() {
 }
 
 function myFunction2() {
-    const element = document.getElementsByTagName("about-me");
-    if (element.length > 0) {
-        document.getElementById("demo").innerHTML = '<br>The "About Me" section is: <br>' + aboutMeText;
-    } else {
-        document.getElementById("demo").innerHTML = "The 'About Me' section was not found.";
-    }
-}
+     const aboutMeSection = document.getElementById("about-me"); 
+     if (aboutMeSection) { 
+         const aboutMeText = aboutMeSection.innerText; 
+         document.getElementById("demo").innerHTML = '<br>The "About Me" section is: <br>' + aboutMeText;
+     } else {
+         document.getElementById("demo").innerHTML = "The 'About Me' section was not found.";
+     }
+ }
