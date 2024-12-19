801af1cc3fb919de6abc7af6e0c0fef5748789a9
Christian Esguerra <ciesguerra2@student.apc.edu.ph>
2024-12-19 21:30:49 +0800

    testing fix

--- home/js/myScript.js
+++ home/js/myScript.js
@@ -3,6 +3,6 @@ function myFunction() {
 }
 
 function myFunction2() {
-    const element = document.getElementsByTagName("about-me");
+    const element = document.getElementsByTagName("p");
     document.getElementById("demo").innerHTML = '<br> The text in first paragraph (index 0) is: <br>' + element[0].innerHTML;
 }
